e8ea9ad56a74cefd4c9e390136796d8973bfadb8
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-30 14:12:13 -0300

    Fix rubocop linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new()
+    @user = User.new
   end
 
   def create
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
-    params.require(:user).permit(:username,:email,:password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
