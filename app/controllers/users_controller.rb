a0357e9d53761dda758a6ba624494190eb9bcbb2
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-30 14:05:52 -0300

    Finish edit page and error showcase

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
 
     if @user.update(user_params)
       redirect_to new_user_path
